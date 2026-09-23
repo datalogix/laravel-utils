1bfbfac5771058c25465aa5b1037170b45331c69
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2026-09-23 11:09:53 -0300

    Update tests

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -4,6 +4,13 @@ namespace Datalogix\Utils\Tests;
 
 class HelpersTest extends TestCase
 {
+    public function test_helpers_file_is_loadable_multiple_times()
+    {
+        require __DIR__.'/../src/helpers.php';
+
+        $this->assertTrue(function_exists('statesBR'));
+    }
+
     public function test_states_br()
     {
         $this->assertIsArray(statesBR());
